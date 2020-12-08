ef9de3bdfe3af427231ff0cb15b2b364d83e5b7d
chillyproductions <54706474+chillyproductions@users.noreply.github.com>
2020-12-08 21:32:23 +0200

    8.12

---
+++ BALListic motion (get it)/launcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class launcher : MonoBehaviour
+{
+
+    void Start()
+    {
+        double v0 = 10;
+        double ang = 45;
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        double F = rb.mass * v0 * 60;
+
+        double x_force = Math.Cos((ang * Math.PI) / 180) * F;
+        double y_force = Math.Sin((ang * Math.PI) / 180) * F;
+
+        rb.AddForce(Convert.ToSingle(1f * x_force), Convert.ToSingle(1f * y_force), 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transform.position.x + ",  " + transform.position.y);
+    }
+}
