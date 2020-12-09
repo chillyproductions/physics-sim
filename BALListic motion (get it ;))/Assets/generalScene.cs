e7b8573f769636d6cf6b143bb1c06820c32483ff
Kartiv <harelchibong@gmail.com>
2020-12-09 10:51:05 +0200

    Some Final Touches
    Added numbers on the number lines, added weird cannon for some reason, set the ui and shit to fit the aspect ratio so build doesnt look weird

--- BALListic motion (get it ;))/Assets/generalScene.cs
+++ BALListic motion (get it ;))/Assets/generalScene.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 public class generalScene : MonoBehaviour
 {
@@ -14,16 +15,34 @@ public class generalScene : MonoBehaviour
 
     void Start()
     {
+        GameObject yLabel = GameObject.Find("yLabel1");
+        GameObject xLabel = GameObject.Find("xLabel1");
+        //TextMesh yLabel1 = yLabel.GetComponent<TextMesh>();
         Vector3 Temp = theOneTwo.transform.position;
-        for(int i=2; i<=50; i++)
+        for(int i=2; i<=54; i++)
         {
-            if (i < 25)
+            if (i < 30)
             {
                 GameObject theOther = GameObject.Instantiate(theOne);
                 theOther.transform.position = new Vector3(0, i, 0);
+                GameObject otherLabel = GameObject.Instantiate(yLabel);
+                otherLabel.GetComponent<TextMesh>().text = i.ToString();
+                otherLabel.transform.position = new Vector3(yLabel.transform.position.x, i, yLabel.transform.position.z);
             }
             GameObject theOtherOne = GameObject.Instantiate(theOneTwo);
             theOtherOne.transform.position = new Vector3(i, Temp.y, 0);
+
+            GameObject otherLabel2 = GameObject.Instantiate(xLabel);
+            otherLabel2.GetComponent<TextMesh>().text = i.ToString();
+            if (i < 22)
+            {
+                otherLabel2.transform.position = new Vector3(i + (float)0.5 - 1, xLabel.transform.position.y, xLabel.transform.position.z);
+            }
+            else if(i<36)
+            {
+                otherLabel2.transform.position = new Vector3(i + (float)0.70 - 1, xLabel.transform.position.y, xLabel.transform.position.z);
+            }
+            else { otherLabel2.transform.position = new Vector3(i, xLabel.transform.position.y, xLabel.transform.position.z); }
         }
     }
 
@@ -64,14 +83,18 @@ public class generalScene : MonoBehaviour
     {
         string[] str_arr = Array.ConvertAll(xArr.ToArray(), x => x.ToString());
 
-        System.IO.File.WriteAllLines(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\uwuXXXARRAYdinosaurfoldersenpai.txt", str_arr);
+        System.IO.File.WriteAllLines(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) +@"\XArray", str_arr);
 
     }
     public void downloadY()
     {
         string[] str_arr = Array.ConvertAll(yArr.ToArray(), x => x.ToString());
 
-        System.IO.File.WriteAllLines(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\uwuYYYARRAYdinosaurfoldersenpai.txt", str_arr);
+        System.IO.File.WriteAllLines(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\YArray", str_arr);
 
+    }
+    public void returnBack()
+    {
+        SceneManager.LoadScene(0);
     }
 }
